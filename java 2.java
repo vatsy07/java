20b892d651bacee7336beffaf363c5b9ffdab6cd
vatsy07 <shahvats7@gmail.com>
2026-01-08 15:47:24 +0530

    Add files via upload

---
+++ java 2.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();  
+        int b = sc.nextInt();   
+
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
+
